060efdfe6248c35af4d5ba86f791688b5ecb2600
Cristian Henzel <oss@web-tm.com>
2010-11-09 12:46:50 +0200

    ClipIt-v1.1.0-09112010001-beta
    + Added: Option to show index numbers in history.
    + Fixed: Search window now gets properly focused when launched with the hotkey.
    + Fixed: Search window is now resizable.
    + Fixed: Double casting when calling 'item_selected'.
    + Fixed: Removed some unnecessary comments and fixed some of the others.
    + Fixed: Moved search dialog and functions to manage.c, as this dialog will
    		become "Manage Clipboard" in the next version.

--- src/main.h
+++ src/main.h
@@ -35,6 +35,7 @@ typedef struct
   gboolean  use_copy;         /* Use copy */
   gboolean  use_primary;      /* Use primary */
   gboolean  synchronize;      /* Synchronize copy and primary */
+  gboolean  show_indexes;     /* Show index numbers in history menu */
   
   gboolean  save_history;     /* Save history */
   gint      history_limit;    /* Items in history */
