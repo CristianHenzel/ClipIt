23a3d91f4c29b7875a85f36861f8e20dcea02106
Cristian Henzel <oss@rspwn.com>
2011-05-25 08:48:36 +0300

    ClipIt-1.4.1-20110525001
    + Fixed: Changed history file to new type.

--- src/history.c
+++ src/history.c
@@ -26,11 +26,19 @@
 #include <glib.h>
 #include <gtk/gtk.h>
 #include <string.h>
+#include <stdlib.h>
 #include "main.h"
 #include "utils.h"
 #include "history.h"
 
 GSList *history;
+
+static history_item *initialize_history_item()
+{
+	history_item *hitem = g_new0(history_item, 1);
+	hitem->is_static = 0;
+	return hitem;
+}
 
 /* Returns TRUE if text should be excluded from history */
 static gboolean is_excluded(gchar *text)
@@ -79,27 +87,62 @@ void read_history()
 	FILE *history_file = fopen(history_path, "rb");
 	g_free(history_path);
 	/* Check that it opened and begin read */
-	if (history_file)
-	{
+	if (history_file) {
 		/* Read the size of the first item */
-		gint size;
+		int size;
+		size_t fread_return;
 		if (fread(&size, 4, 1, history_file) != 1)
 			size = 0;
-		/* Continue reading until size is 0 */
-		while (size)
-		{
-			/* Malloc according to the size of the item */
-			gchar *item = (gchar*)g_malloc(size + 1);
-			/* Read item and add ending character */
-			size_t fread_return;
-			fread_return = fread(item, size, 1, history_file);
-			item[size] = '\0';
-			/* Prepend item and read next size */
-			history_item *hitem = g_new0 (history_item, 1);
-			hitem->content = item;
-			history = g_slist_prepend(history, hitem);
+		if (size == -1) {
+			/* If size is -1, we are using the new filetype introduced in 1.4.1 */
+			/* We currently aren't using the extra data fields */
+			char extra_data[64];
+			int data_type;
+			fread_return = fread(&extra_data, 64, 1, history_file);
 			if (fread(&size, 4, 1, history_file) != 1)
 				size = 0;
+			if (fread(&data_type, 4, 1, history_file) != 1)
+				data_type = 0;
+			while (size && data_type) {
+				switch (data_type) {
+					case 1: {
+						gchar *item = (gchar*)g_malloc(size + 1);
+						fread_return = fread(item, size, 1, history_file);
+						item[size] = '\0';
+						history_item *hitem = initialize_history_item();
+						hitem->content = item;
+						history = g_slist_prepend(history, hitem);
+						break;
+					}
+					case 2: {
+						int read_static;
+						history_item *hitem = history->data;
+						fread_return = fread(&read_static, size, 1, history_file);
+						hitem->is_static = read_static;
+						printf("static: %d\n", read_static);
+						break;
+					}
+				}
+				if (fread(&size, 4, 1, history_file) != 1)
+					size = 0;
+				if (fread(&data_type, 4, 1, history_file) != 1)
+					data_type = 0;
+			}
+		} else {
+			/* Continue reading until size is 0 */
+			while (size) {
+				/* Malloc according to the size of the item */
+				gchar *item = (gchar*)g_malloc(size + 1);
+				/* Read item and add ending character */
+				fread_return = fread(item, size, 1, history_file);
+				item[size] = '\0';
+				/* Prepend item and read next size */
+				history_item *hitem = initialize_history_item();
+				hitem->content = item;
+				history = g_slist_prepend(history, hitem);
+				if (fread(&size, 4, 1, history_file) != 1)
+					size = 0;
+			}
 		}
 		/* Close file and reverse the history to normal */
 		fclose(history_file);
@@ -121,24 +164,36 @@ void save_history()
 		FILE *history_file = fopen(history_path, "wb");
 		g_free(history_path);
 		/* Check that it opened and begin write */
-		if (history_file)
-		{
+		if (history_file) {
 			GSList *element;
 			/* Write each element to a binary file */
-			for (element = history; element != NULL; element = element->next)
-			{
+			int i;
+			for (i=1; i<=17; i++) {
+				int write_val = -1;
+				fwrite(&write_val, 4, 1, history_file);
+			}
+			for (element = history; element != NULL; element = element->next) {
 				/* Create new GString from element data, write its
 				 * length (size) to file followed by the element
 				 * data itself
 				 */
+				int write_val = 1;
 				history_item *elem_data = element->data;
 				GString *item = g_string_new((gchar*)elem_data->content);
+				int write_static = elem_data->is_static;
 				fwrite(&(item->len), 4, 1, history_file);
+				fwrite(&write_val, 4, 1, history_file);
 				fputs(item->str, history_file);
+				write_val = 4;
+				fwrite(&write_val, 4, 1, history_file);
+				write_val = 2;
+				fwrite(&write_val, 4, 1, history_file);
+				fwrite(&write_static, 4, 1, history_file);
 				g_string_free(item, TRUE);
 			}
 			/* Write 0 to indicate end of file */
 			gint end = 0;
+			fwrite(&end, 4, 1, history_file);
 			fwrite(&end, 4, 1, history_file);
 			fclose(history_file);
 		}
@@ -154,13 +209,11 @@ void save_history()
 /* Checks if item should be included in history and calls append */
 void check_and_append(gchar *item)
 {
-	if (item)
-	{
+	if (item) {
 		/* if item is too big, we don't include it in the history */
 		if(strlen(item) > ENTRY_MAX_SIZE)
 			return;
 		GtkClipboard *clip = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
-		/* Prepend new item */
 		/* Check if we have URIs */
 		gchar **arr = gtk_clipboard_wait_for_uris(clip);
 		if(arr != NULL) {
@@ -170,9 +223,7 @@ void check_and_append(gchar *item)
 		}
 		g_strfreev(arr);
 		if(!is_excluded(item))
-		{
 			append_item(item);
-		}
 	}
 }
 
@@ -188,13 +239,11 @@ void append_item(gchar *item)
 /* Truncates history to history_limit items */
 void truncate_history()
 {
-	if (history)
-	{
+	if (history) {
 		/* Shorten history if necessary */
 		GSList *last_possible_element = g_slist_nth(history,
 						prefs.history_limit - 1);
-		if (last_possible_element)
-		{
+		if (last_possible_element) {
 			/* Free last posible element and subsequent elements */
 			g_slist_free(last_possible_element->next);
 			last_possible_element->next = NULL;
@@ -207,20 +256,16 @@ void truncate_history()
 /* Returns pointer to last item in history */
 gpointer get_last_item()
 {
-	if (history)
-	{
+	if (history) {
 		history_item *elem_data = history->data;
-		if (elem_data->content)
-		{
+		if (elem_data->content) {
 			/* Return the last element */
 			gpointer last_item = elem_data->content;
 			return last_item;
 		}
-		else
-			return NULL;
-	}
-	else
 		return NULL;
+	}
+	return NULL;
 }
 
 /* Deletes duplicate item in history */
@@ -228,11 +273,9 @@ void delete_duplicate(gchar *item)
 {
 	GSList *element;
 	/* Go through each element compare each */
-	for (element = history; element != NULL; element = element->next)
-	{
+	for (element = history; element != NULL; element = element->next) {
 		history_item *elem_data = element->data;
-		if (g_strcmp0((gchar*)elem_data->content, item) == 0)
-		{
+		if (g_strcmp0((gchar*)elem_data->content, item) == 0) {
 			g_free(elem_data->content);
 			history = g_slist_delete_link(history, element);
 			break;
