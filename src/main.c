eac0e86d0f9701d67e34a8305ee80dca131c35e0
Cristian Henzel <oss@rspwn.com>
2011-05-26 15:38:12 +0300

    ClipIt-1.4.1-20110526005
    + Added: Added options for static items in menu.

--- src/main.c
+++ src/main.c
@@ -60,10 +60,11 @@ static gboolean actions_lock = FALSE;
 prefs_t prefs = {DEF_USE_COPY,         DEF_USE_PRIMARY,      DEF_SYNCHRONIZE,
                  DEF_AUTOMATIC_PASTE,  DEF_SHOW_INDEXES,     DEF_SAVE_URIS,
                  DEF_USE_RMB_MENU,     DEF_SAVE_HISTORY,     DEF_HISTORY_LIMIT,
-                 DEF_ITEMS_MENU,       DEF_HYPERLINKS_ONLY,  DEF_CONFIRM_CLEAR,
-                 DEF_SINGLE_LINE,      DEF_REVERSE_HISTORY,  DEF_ITEM_LENGTH,
-                 DEF_ELLIPSIZE,        INIT_HISTORY_KEY,     INIT_ACTIONS_KEY,
-                 INIT_MENU_KEY,        INIT_SEARCH_KEY,      DEF_NO_ICON};
+                 DEF_ITEMS_MENU,       DEF_STATICS_SHOW,     DEF_STATICS_ITEMS,
+                 DEF_HYPERLINKS_ONLY,  DEF_CONFIRM_CLEAR,    DEF_SINGLE_LINE,
+                 DEF_REVERSE_HISTORY,  DEF_ITEM_LENGTH,      DEF_ELLIPSIZE,
+                 INIT_HISTORY_KEY,     INIT_ACTIONS_KEY,     INIT_MENU_KEY,
+                 INIT_SEARCH_KEY,      DEF_NO_ICON};
 
 /* Called every CHECK_INTERVAL seconds to check for new items */
 static gboolean item_check(gpointer data)
@@ -457,58 +458,58 @@ static gboolean show_actions_menu(gpointer data)
   return FALSE;
 }
 
-static gboolean menu_key_pressed(GtkWidget *widget, GdkEventKey *event, gpointer user_data)
+static gboolean menu_key_pressed(GtkWidget *history_menu, GdkEventKey *event, gpointer user_data)
 {
   switch (event->keyval) {
     case XK_1:
     case XK_KP_1:
-      item_selected((GtkMenuItem*)widget, GINT_TO_POINTER(0));
-      gtk_widget_destroy(widget);
+      item_selected((GtkMenuItem*)history_menu, GINT_TO_POINTER(0));
+      gtk_widget_destroy(history_menu);
       break;
     case XK_2:
     case XK_KP_2:
-      item_selected((GtkMenuItem*)widget, GINT_TO_POINTER(1));
-      gtk_widget_destroy(widget);
+      item_selected((GtkMenuItem*)history_menu, GINT_TO_POINTER(1));
+      gtk_widget_destroy(history_menu);
       break;
     case XK_3:
     case XK_KP_3:
-      item_selected((GtkMenuItem*)widget, GINT_TO_POINTER(2));
-      gtk_widget_destroy(widget);
+      item_selected((GtkMenuItem*)history_menu, GINT_TO_POINTER(2));
+      gtk_widget_destroy(history_menu);
       break;
     case XK_4:
     case XK_KP_4:
-      item_selected((GtkMenuItem*)widget, GINT_TO_POINTER(3));
-      gtk_widget_destroy(widget);
+      item_selected((GtkMenuItem*)history_menu, GINT_TO_POINTER(3));
+      gtk_widget_destroy(history_menu);
       break;
     case XK_5:
     case XK_KP_5:
-      item_selected((GtkMenuItem*)widget, GINT_TO_POINTER(4));
-      gtk_widget_destroy(widget);
+      item_selected((GtkMenuItem*)history_menu, GINT_TO_POINTER(4));
+      gtk_widget_destroy(history_menu);
       break;
     case XK_6:
     case XK_KP_6:
-      item_selected((GtkMenuItem*)widget, GINT_TO_POINTER(5));
-      gtk_widget_destroy(widget);
+      item_selected((GtkMenuItem*)history_menu, GINT_TO_POINTER(5));
+      gtk_widget_destroy(history_menu);
       break;
     case XK_7:
     case XK_KP_7:
-      item_selected((GtkMenuItem*)widget, GINT_TO_POINTER(6));
-      gtk_widget_destroy(widget);
+      item_selected((GtkMenuItem*)history_menu, GINT_TO_POINTER(6));
+      gtk_widget_destroy(history_menu);
       break;
     case XK_8:
     case XK_KP_8:
-      item_selected((GtkMenuItem*)widget, GINT_TO_POINTER(7));
-      gtk_widget_destroy(widget);
+      item_selected((GtkMenuItem*)history_menu, GINT_TO_POINTER(7));
+      gtk_widget_destroy(history_menu);
       break;
     case XK_9:
     case XK_KP_9:
-      item_selected((GtkMenuItem*)widget, GINT_TO_POINTER(8));
-      gtk_widget_destroy(widget);
+      item_selected((GtkMenuItem*)history_menu, GINT_TO_POINTER(8));
+      gtk_widget_destroy(history_menu);
       break;
     case XK_0:
     case XK_KP_0:
-      item_selected((GtkMenuItem*)widget, GINT_TO_POINTER(9));
-      gtk_widget_destroy(widget);
+      item_selected((GtkMenuItem*)history_menu, GINT_TO_POINTER(9));
+      gtk_widget_destroy(history_menu);
       break;
   }
   return FALSE;
@@ -519,6 +520,7 @@ static GtkWidget *create_history_menu(GtkWidget *history_menu)
 	GtkWidget *menu_item, *item_label;
 	history_menu = gtk_menu_new();
 	g_signal_connect((GObject*)history_menu, "key-press-event", (GCallback)menu_key_pressed, NULL);
+
 	/* Items */
 	if ((history != NULL) && (history->data != NULL))
 	{
