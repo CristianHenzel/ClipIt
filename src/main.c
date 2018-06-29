949bf67c51424f93fac249206c5cd3ff28097412
Cristian Henzel <CristianHenzel@users.noreply.github.com>
2018-06-29 14:29:16 +0300

    Remove use of gtk_window_get_pointer
    Closes #93
    Closes #55

--- src/main.c
+++ src/main.c
@@ -143,7 +143,17 @@ static gboolean item_check(gpointer data) {
   else
   {
     GdkModifierType button_state;
-    gdk_window_get_pointer(NULL, NULL, NULL, &button_state);
+    GdkScreen *screen = gdk_screen_get_default();
+    if (screen)
+    {
+      GdkDisplay *display = gdk_screen_get_display(screen);
+      GdkWindow *window = gdk_screen_get_root_window(screen);
+      GdkSeat *seat = gdk_display_get_default_seat(display);
+
+      gdk_window_get_device_position(window, gdk_seat_get_pointer(seat), NULL,
+          NULL, &button_state);
+    }
+
     /* Proceed if mouse button not being held */
     if ((primary_temp != NULL) && !(button_state & GDK_BUTTON1_MASK))
     {
